0a400a9f7269e01b89f81c65ef35492ab6ff4481
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2021-12-23 18:45:32 +0530

    Add files via upload

--- chatapp.js
+++ chatapp.js
@@ -1,6 +1,6 @@
 function addUser()
 {
     window.location = "chatapp_rooms.html";
-    username = document.getElementById("username_input");
-    localStorage.setItem("Username: ", username);
+    username = document.getElementById("username_input").value;
+    localStorage.setItem("Username", username);
 }
